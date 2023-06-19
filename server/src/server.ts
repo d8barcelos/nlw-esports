bedc79db59d7d49899a981571c616f2de1134d42
Diogo <d8barcelos@gmail.com>
2023-06-19 10:12:43 -0300

    commit1

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express()
+
+app.get('/ads', (request, response) => {
+  return response.json([
+    { id : 1, name: '1' },
+    { id : 2, name: '2' },
+    { id : 3, name: '3' },
+    { id : 4, name: '4' }
+  ])
+})
+
+app.listen(3333)
