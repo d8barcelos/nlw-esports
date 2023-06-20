c789e166cfabc8cdbbd45ce07bd2e6e910a1dfd2
Diogo <d8barcelos@gmail.com>
2023-06-20 20:03:45 -0300

    commit

---
+++ mobile/src/components/background/index.tsx
@@ -0,0 +1,17 @@
+import { ImageBackground } from 'react-native';
+
+import { styles } from './styles';
+
+import backgroundImg from '../../assets/background-galaxy.png';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function background({ children }: Props) {
+  return (
+      <ImageBackground source={backgroundImg} defaultSource={backgroundImg} style={styles.container}>
+        {children}
+      </ImageBackground>
+  );
+}
